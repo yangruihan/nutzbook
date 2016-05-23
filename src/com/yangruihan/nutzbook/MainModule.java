bcc9dd173fb07c9285447852c9e5fb6688106621
C.y. <yangruihan@vip.qq.com>
2016-05-23 17:18:23 +0800

    Nutzbook v3.2 自定义动作链 添加自定义 processor

--- src/com/yangruihan/nutzbook/MainModule.java
+++ src/com/yangruihan/nutzbook/MainModule.java
@@ -1,5 +1,6 @@
 package com.yangruihan.nutzbook;
 
+import org.nutz.mvc.annotation.ChainBy;
 import org.nutz.mvc.annotation.Fail;
 import org.nutz.mvc.annotation.IocBy;
 import org.nutz.mvc.annotation.Localization;
@@ -18,6 +19,7 @@ import org.nutz.mvc.ioc.provider.ComboIocProvider;
 @Ok("json:full") // 配置默认成功返回值
 @Fail("jsp:jsp.500") // 配置默认失败返回值
 @Localization(value="msg/", defaultLocalizationKey="zh-CN") // 配置国际化
+@ChainBy(args="mvc/nutzbook-mvc-chain.js") // 配置动作链
 public class MainModule {
 
 }
